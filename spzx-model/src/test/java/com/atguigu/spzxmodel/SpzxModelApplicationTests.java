481beaf5bd5bf4cabc8ef4135599214f5e906f50
B510lms <3020506015@qq.com>
2026-05-26 00:50:57 +0800

    init: 项目初始化

---
+++ spzx-model/src/test/java/com/atguigu/spzxmodel/SpzxModelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.spzxmodel;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpzxModelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ src/main/java/com/atguigu/spzxparent/SpzxParentApplication.java
@@ -0,0 +1,13 @@
+package com.atguigu.spzxparent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpzxParentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpzxParentApplication.class, args);
+    }
+
+}
