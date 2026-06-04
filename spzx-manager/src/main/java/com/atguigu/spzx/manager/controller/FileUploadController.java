6766b26969d33b84f6c57053227fa3c144f54d91
B510lms <3020506015@qq.com>
2026-06-04 21:29:31 +0800

    feat: 分类管理 品牌管理 分类品牌管理

--- spzx-manager/src/main/java/com/atguigu/spzx/manager/controller/FileUploadController.java
+++ spzx-manager/src/main/java/com/atguigu/spzx/manager/controller/FileUploadController.java
@@ -4,12 +4,14 @@ import com.atguigu.spzx.manager.service.FileUploadService;
 import com.atguigu.spzx.model.vo.common.Result;
 import com.atguigu.spzx.model.vo.common.ResultCodeEnum;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+@Tag(name="文件上传")
 @RestController
 @RequestMapping("/admin/system")
 public class FileUploadController {
